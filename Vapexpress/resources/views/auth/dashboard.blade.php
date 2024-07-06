512590bd58cbbd0f3a249014ea8e3a338d438d79
Miguel Vega Molina <mvegmol24@gmail.com>
2024-07-06 18:22:03 +0200

    UPDATE Dashboard view
    Se ha actalizado la vista del dashboard del cliente.

--- Vapexpress/resources/views/auth/dashboard.blade.php
+++ Vapexpress/resources/views/auth/dashboard.blade.php
@@ -1,24 +1,21 @@
 @extends('template.app')
 
 @section('content')
-    <div class="container mx-auto my-10">
-        <div class="flex justify-center">
-            <div class="w-full max-w-md">
-                <div class="bg-white shadow-md rounded-lg">
-                    <div class="bg-primary text-white px-6 py-4 rounded-t-lg">
-                        Dashboard
-                    </div>
+    <div class="flex flex-col justify-center pb-6 py-2 sm:px-6 lg:px-8 px-6">
 
-                    <div class="p-6">
-                        @if (session('status'))
-                            <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4"
-                                role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        You are logged in!
-                    </div>
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+            <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold ">
+                Dashboard
+            </h2>
+        </div>
+        <div class="mt-4  sm:mx-auto sm:w-full sm:max-w-md">
+            @if (session('status'))
+                <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4" role="alert">
+                    {{ session('status') }}
                 </div>
+            @endif
+            <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                Te ha registrado correctamente!
             </div>
         </div>
     </div>
