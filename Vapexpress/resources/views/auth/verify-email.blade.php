a87dbdcb39d89a847b026085470df479edc45d70
Miguel Vega Molina <mvegmol24@gmail.com>
2024-07-06 18:17:48 +0200

    UPDATE Verify-email view
    Se ha actualizado la vista para poder reenviar el correo electrónico para verificar la cuenta en Vapexpress.

--- Vapexpress/resources/views/auth/verify-email.blade.php
+++ Vapexpress/resources/views/auth/verify-email.blade.php
@@ -1,35 +1,39 @@
 @extends('template.app')
 
 @section('content')
-    <div class="container mx-auto p-4 my-6">
-        <div class="flex justify-center">
-            @if (session('status'))
-                <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4"
-                    role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif
-            <div class="w-full md:w-2/3 lg:w-1/2">
-                <div class="bg-white shadow-md rounded-lg">
-                    <div class="bg-primary text-white px-6 py-3 rounded-t-lg font-semibold">
-                        {{ __('Email Verification') }}
-                    </div>
+    <div class="flex flex-col justify-center pb-6 py-2 sm:px-6 lg:px-8 px-6">
+        @if (session('status'))
+            <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+                Enlace de verificación enviado.
+            </div>
+        @endif
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+            <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold ">
+                Correo de verificación
+            </h2>
+        </div>
+        <div class="mt-4  sm:mx-auto sm:w-full sm:max-w-md">
+            <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <form method="POST" action="{{ route('verification.send') }}">
+                    @csrf
+                    <p class="mt-2 text-center text-sm leading-5 text-blue-600 max-w">
+                        Debes verificar tu dirección de correo electrónico. Por favor, revisa tu correo electrónico para
+                        encontrar
+                        un enlace de verificación.
+                    </p>
 
-                    <div class="p-6">
-                        <p>You must verify your email address. Please, check your email for a verification link</p>
-                        <form method="POST" action="{{ route('verification.send') }}">
-                            @csrf
-
-                            <div class="flex items-center justify-end mt-4">
-                                <button type="submit"
-                                    class="bg-primary  hover:bg-tertiary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                    value="Resend">
-                                    {{ __('Resend Email') }}
-                                </button>
-                            </div>
-                        </form>
+                    <div class="mt-3">
+                        <span class="block w-full rounded-md shadow-sm">
+                            <button type="submit"
+                                class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md
+                                 text-white bg-navbar hover:bg-text_principal focus:outline-none
+                                  focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+                                Reenviar Correo
+                            </button>
+                        </span>
                     </div>
-                </div>
+                </form>
+
             </div>
         </div>
     </div>
