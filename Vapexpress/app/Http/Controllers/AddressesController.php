1f0ff70d6b323ef5422443b1f1715e3eedc5584e
Miguel Vega Molina <mvegmol24@gmail.com>
2024-07-07 12:20:54 +0200

    ADD Address update logic & view #1
    Se ha añadido la lógica de acutalizar la direcciones disponibles de un cliente.

--- Vapexpress/app/Http/Controllers/AddressesController.php
+++ Vapexpress/app/Http/Controllers/AddressesController.php
@@ -99,27 +99,49 @@ class AddressesController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(int  $id)
     {
         try {
-            DB::beginTransaction();
-
-            DB::commit();
+            if (Auth::user()->rol != 'admin') {
+                DB::beginTransaction();
+                $address = Address::findOrFail($id);
+                DB::commit();
+                return view('addresses.edit', compact('address'));
+            } else {
+                return view('client.profile')->with('error', 'El administrador no puede tener direcciones');
+            }
         } catch (\Exception $e) {
+            dd($e);
             DB::rollBack();
-            return back()->with('error', 'Failed to fetch addresses.');
+            return back()->with('error', 'Error al buscar la dirección.');
         }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Address $address)
     {
+        $validatedData = $request->validate([
+            'full_name' => 'required|string|max:255',
+            'contact_phone' => 'required|string|max:9|min:9',
+            'direction' => 'required|string|max:255',
+            'city' => 'required|string|max:30',
+            'province' => 'required|string|max:30',
+            'zip_code' => 'required|string|max:5',
+        ]);
         try {
             DB::beginTransaction();
 
+            $address->full_name = $validatedData['full_name'];
+            $address->contact_phone = $validatedData['contact_phone'];
+            $address->direction = $validatedData['direction'];
+            $address->city = $validatedData['city'];
+            $address->province = $validatedData['province'];
+            $address->zip_code = $validatedData['zip_code'];
+            $address->save();
             DB::commit();
+            return redirect()->route('addresses.index')->with('success', 'Dirección actualizada correctamente.');
         } catch (\Exception $e) {
             DB::rollBack();
             return back()->with('error', 'Failed to fetch addresses.');
---
+++ Vapexpress/resources/views/addresses/edit.blade.php
@@ -0,0 +1,136 @@
+@extends('template.app')
+
+
+@section('content')
+    <div class="flex flex-col justify-center pb-6 py-2 sm:px-6 lg:px-8 px-6">
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+            <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold ">
+                Modificar Dirección
+            </h2>
+
+        </div>
+
+
+        <div class="mt-4  sm:mx-auto sm:w-full sm:max-w-md">
+            <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <form method="POST" action="{{ route('addresses.update', ['address' => $address->id]) }}">
+                    @method('PUT')
+                    @csrf
+
+                    <div>
+                        <label for="full_name" class="block text-sm font-medium leading-5  text-navbar">Nombre de
+                            contacto</label>
+
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                            <input id="full_name" name="full_name" value="{{ $address->full_name }}" type="text" required
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 
+                                focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5
+                                @error('full_name') border-red-500 @enderror">
+                        </div>
+                        @error('full_name')
+                            <span class="text-red-500 text-sm mt-2" role="alert">
+                                <strong>Nombre incorrecto.</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="mt-6">
+                        <label for="contact_phone" class="block text-sm font-medium leading-5  text-navbar">Número de
+                            Contacto</label>
+
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                            <input id="contact_phone" name="contact_phone" value="{{ $address->contact_phone }}"
+                                type="text" required
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 
+                                focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5
+                                @error('contact_phone') border-red-500 @enderror">
+                        </div>
+                        @error('contact_phone')
+                            <span class="text-red-500 text-sm mt-2" role="alert">
+                                <strong>Número de teléfono incorrecto.</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="mt-6">
+                        <label for="direction" class="block text-sm font-medium leading-5  text-navbar">Calle</label>
+
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                            <input id="direction" name="direction" value="{{ $address->direction }}" type="text" required
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 
+                                focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5
+                                @error('direction') border-red-500 @enderror">
+                        </div>
+                        @error('direction')
+                            <span class="text-red-500 text-sm mt-2" role="alert">
+                                <strong>Calle incorrecta.</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                    <div class="mt-6">
+                        <label for="city" class="block text-sm font-medium leading-5 text-navbar">Ciudad</label>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <input id="city" name="city" type="text" required value="{{ $address->city }}"
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md 
+                                placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 
+                                transition duration-150 ease-in-out sm:text-sm sm:leading-5
+                                 @error('city') border-red-500 @enderror">
+
+                        </div>
+                        @error('city')
+                            <span class="text-red-500 text-sm mt-2" role="alert">
+                                <strong>Ciudad incorrecta.</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+
+                    <div class="mt-6">
+                        <label for="province" class="block text-sm font-medium leading-5 text-navbar">Provincia</label>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <input id="province" name="province" type="text" required value="{{ $address->province }}"
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md 
+                                placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 
+                                transition duration-150 ease-in-out sm:text-sm sm:leading-5
+                                 @error('province') border-red-500 @enderror">
+
+                        </div>
+                        @error('province')
+                            <span class="text-red-500 text-sm mt-2" role="alert">
+                                <strong>Provincia incorrecta.</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                    <div class="mt-6">
+                        <label for="zip_code" class="block text-sm font-medium leading-5 text-navbar">Código Postal</label>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <input id="zip_code" name="zip_code" type="text" required value="{{ $address->zip_code }}"
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md 
+                                placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 
+                                transition duration-150 ease-in-out sm:text-sm sm:leading-5
+                                 @error('zip_code') border-red-500 @enderror">
+
+                        </div>
+                        @error('zip_code')
+                            <span class="text-red-500 text-sm mt-2" role="alert">
+                                <strong>Código Postal incorrecto.</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                    <div class="mt-6">
+                        <span class="block w-full rounded-md shadow-sm">
+                            <button type="submit"
+                                class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md
+                                 text-white bg-navbar hover:bg-text_principal focus:outline-none
+                                  focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+                                Modificar
+                            </button>
+                        </span>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+@endsection
