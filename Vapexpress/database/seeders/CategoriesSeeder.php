bd4789c33e314837b0a01d218101073b5f96b7fc
Miguel Vega Molina <mvegmol24@gmail.com>
2024-07-22 18:41:35 +0200

    Update CategoriesSeeder.php
    Se ha actualizado las seeders de categorías para tener categorías reales y no de prueba.

--- Vapexpress/database/seeders/CategoriesSeeder.php
+++ Vapexpress/database/seeders/CategoriesSeeder.php
@@ -14,12 +14,26 @@ class CategoriesSeeder extends Seeder
      */
     public function run(): void
     {
-        $faker = Faker::create();
+        $categories = [
+            ['name' => 'Vaper Recargable', 'description' => 'Dispositivos de vapeo recargables con baterías duraderas y múltiples opciones de sabor.'],
+            ['name' => 'Vaper Desechables', 'description' => 'Vapers de un solo uso, perfectos para probar nuevos sabores sin compromiso.'],
+            ['name' => 'Accesorios', 'description' => 'Accesorios esenciales para personalizar y mantener tus dispositivos de vapeo.'],
+            ['name' => 'Vaper', 'description' => 'Variedad de vapers para todos los niveles de experiencia y preferencias.'],
+            ['name' => 'Sales de Nicotina', 'description' => 'E-líquidos con sales de nicotina para una experiencia de vapeo más suave y satisfactoria.'],
+            ['name' => 'E-líquidos', 'description' => 'Amplia gama de e-líquidos con diversos sabores y niveles de nicotina.'],
+            ['name' => 'Kits de Inicio', 'description' => 'Paquetes completos para aquellos que se inician en el mundo del vapeo.'],
+            ['name' => 'Mods y Baterías', 'description' => 'Dispositivos avanzados y baterías de alta calidad para vapeadores experimentados.'],
+            ['name' => 'Atomizadores y Tanques', 'description' => 'Componentes de alta calidad para mejorar la experiencia de vapeo y personalización.'],
+            ['name' => 'Cargadores y Estuches', 'description' => 'Soluciones de carga y almacenamiento para mantener tus dispositivos seguros y listos para usar.'],
+            ['name' => 'Resistencias y Coils', 'description' => 'Variedad de resistencias y coils para un rendimiento óptimo de tus dispositivos de vapeo.'],
+            ['name' => 'Partes de Repuesto', 'description' => 'Piezas de repuesto para mantener tus dispositivos en perfectas condiciones.'],
+        ];
 
-        for ($i = 0; $i < 30; $i++) {
+
+        foreach ($categories as $category) {
             Category::create([
-                'name' => $faker->word,
-                'description' => $faker->sentence,
+                'name' => $category['name'],
+                'description' => $category['description'],
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
